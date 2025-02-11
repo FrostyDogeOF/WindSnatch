fc2d4b97e75f600cfd182dc437855117056c1933
FrostyDogeOF <adrianstephanblaylock@outlook.com>
2025-02-11 17:52:41 -0600

    Initial commit

---
+++ window.gd
@@ -0,0 +1,16 @@
+extends Window
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Settings Menu"):
+		visible = true
+	
+
+func _on_close_requested() -> void:
+	visible = false
